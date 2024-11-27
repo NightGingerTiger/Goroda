9e0ae5e7cb79f4fabb9d523296ab1a5f332bad9f
Rozaliya <tiger.night.tiger@gmail.com>
2024-11-28 00:52:54 +0300

    Добавлена обработка исключений, а так же возврат результата на русском.

--- Goroda.py
+++ Goroda.py
@@ -1,17 +1,20 @@
+from auto_py_to_exe.config import language_hint
 from opencage.geocoder import OpenCageGeocode
 
 
 def get_coordinates(city, key):
-    geocoder = OpenCageGeocode(key)
-    query = city
-    results = geocoder.geocode(query)
-    if results:
-        return results[0]['geometry']['lat'], results[0]['geometry']['lng']
-    else:
-        return "Город не найден"
+    try:
+        geocoder = OpenCageGeocode(key)
+        results = geocoder.geocode(city, language = 'ru')
+        if results:
+            return results[0]['geometry']['lat'], results[0]['geometry']['lng']
+        else:
+            return "Город не найден"
+    except Exception as e:
+        return f"Общая ошибка: {e}"
 
 
 key = '3a5258d829cd48c8a3a51b77e3caec8c'
-city = 'Москва'
+city = 'Сызрань'
 coordinates = get_coordinates(city, key)
 print(f"Координаты города {city}: {coordinates}")
