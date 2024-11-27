3d99dc7d69cc5f13975c65f45b129c23b174b257
Rozaliya <tiger.night.tiger@gmail.com>
2024-11-28 01:39:30 +0300

    Добавлен графический интерфейс.

--- Goroda.py
+++ Goroda.py
@@ -1,4 +1,4 @@
-from auto_py_to_exe.config import language_hint
+from tkinter import *
 from opencage.geocoder import OpenCageGeocode
 
 
@@ -8,15 +8,32 @@ def get_coordinates(city, key):
         results = geocoder.geocode(city, language = 'ru')
         if results:
             lat = round(results[0]['geometry']['lat'], 2)
-            lng = round (results[0]['geometry']['lng']. 2)
+            lng = round (results[0]['geometry']['lng'], 2)
             return f"Широта: {lat}, Долгота: {lng}"
         else:
             return "Город не найден"
     except Exception as e:
         return f"Общая ошибка: {e}"
 
+def show_coordinates(event=None):
+    city = entry.get()
+    coordinates = get_coordinates(city, key)
+    label.config(text=f"Координаты города {city}:\n {coordinates}")
+
+window = Tk()
+window.title("Поиск координат города")
+window.geometry("320x200")
 
 key = '3a5258d829cd48c8a3a51b77e3caec8c'
-city = 'Сызрань'
-coordinates = get_coordinates(city, key)
-print(f"Координаты города {city}: {coordinates}")
+
+entry = Entry()
+entry.pack()
+entry.bind("<Return>", show_coordinates)  # Привязка события нажатия Enter
+
+button = Button(text="Поиск координат", command=show_coordinates)
+button.pack()
+
+label = Label(text="Введите город и нажмите Поиск")
+label.pack()
+
+window.mainloop()
